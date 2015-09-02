9f2af0293e48eb8725f99dc1c1538d70917cf680
unknown <nguyenky@NCS-291214WV01.ncs.corp.int-ads>
2015-09-02 18:46:46 +0800

    init project.

---
+++ src/main/java/my/project/excel/Constants.java
@@ -0,0 +1,14 @@
+package my.project.excel;
+
+public class Constants {
+	private Constants() {
+	}
+
+	public final static String FILE_INPUT_PATH_SAMPLE = "D:/test/input/sample Data.xlsx";
+	public final static String FILE_INPUT_PATH = "D:/test/input/Task 2008 - processed data-1 9th july 2014.xlsx";
+
+	public final static String FILE_OUPUT_PATH = "D:/test/output/result.xlsx";
+
+	public final static String SHEET_MAIN_NAME = "Education";
+	public final static String SHEET_OUTPUT_NAME = "Achievements";
+}
---
+++ src/main/java/my/project/excel/Main.java
@@ -0,0 +1,79 @@
+package my.project.excel;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Iterator;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try {
+			Date startTime = new Date();
+			System.out.println("Started");
+			File inputFile = new File(Constants.FILE_INPUT_PATH);
+			File ouputFile = new File(Constants.FILE_OUPUT_PATH);
+			// file.createNewFile();
+
+			Workbook workbook = new XSSFWorkbook(inputFile);
+			Sheet spreadsheet = workbook.getSheet(Constants.SHEET_MAIN_NAME);
+
+			Workbook outputWorkbook = new XSSFWorkbook();
+			Sheet outputSheet = outputWorkbook
+					.createSheet(Constants.SHEET_OUTPUT_NAME);
+
+			Iterator<Row> rows = spreadsheet.iterator();
+			int rowCount = 0;
+			Row row = null;
+			Row outputRow = null;
+			Cell cell = null;
+			Cell ouputCell = null;
+			while (rows.hasNext()) {
+				row = rows.next();
+				outputRow = outputSheet.createRow(rowCount++);
+
+				Iterator<Cell> cells = row.cellIterator();
+				int cellCount = 0;
+				while (cells.hasNext()) {
+					cell = cells.next();
+					ouputCell = outputRow.createCell(cellCount++);
+					ouputCell.setCellValue(getCellStringValue(cell));
+				}
+			}
+			outputWorkbook.write(new FileOutputStream(ouputFile));
+
+			outputWorkbook.close();
+			workbook.close();
+
+			Date endTime = new Date();
+			long diff = endTime.getTime() - startTime.getTime();
+			long diffSeconds = diff / 1000;
+			System.out.println("Period:" + diffSeconds + " (s)");
+
+			System.out.println("Finish");
+		} catch (IOException | InvalidFormatException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static String getCellStringValue(Cell cell) {
+		String value = "";
+		switch (cell.getCellType()) {
+		case Cell.CELL_TYPE_NUMERIC:
+			value = cell.getNumericCellValue() + "";
+			break;
+		case Cell.CELL_TYPE_STRING:
+			value = cell.getStringCellValue();
+			break;
+		}
+		return value;
+	}
+}
